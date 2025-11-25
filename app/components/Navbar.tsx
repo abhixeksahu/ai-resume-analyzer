55c1ed10868036cfc4b870b214f185c6b399da5e
Abhishek Sahu <kalisahu752@gmail.com>
2025-11-26 02:46:27 +0530

    fix: navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,17 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                <p className="text-2xl font-bold text-gradient">
+                    ResumeAnalyze
+                </p>
             </Link>
             <Link to="/upload" className="primary-button w-fit">
                 Upload Resume
             </Link>
         </nav>
-    )
-}
-export default Navbar
+    );
+};
+export default Navbar;
